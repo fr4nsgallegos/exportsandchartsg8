acc6ae4a44285fa7d34641677d31a9b022979a2c
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-25 19:45:50 -0500

    open pdf doc

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,47 @@
+import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  Future exportPDF() async {
+    final pdf = pw.Document();
+    Uint8List bytes = await pdf.save();
+    print(bytes);
+
+    Directory directory = await getApplicationSupportDirectory();
+    print(directory);
+
+    String filename = "${directory.path}/reportPDF.pdf";
+    print(filename);
+
+    File pdfFile = File(filename);
+    await pdfFile.writeAsBytes(bytes, flush: true);
+
+    OpenFile.open(filename);
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  exportPDF();
+                },
+                child: Text("PDF"),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
