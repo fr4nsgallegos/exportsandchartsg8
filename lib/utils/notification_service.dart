05803cfd32ae3f64beb29232810e9503773fefd0
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-27 11:06:16 -0500

    message on app

--- lib/utils/notification_service.dart
+++ lib/utils/notification_service.dart
@@ -1,10 +1,21 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:logger/logger.dart';
 
 class NotificationService {
   static FirebaseMessaging firebaseMessaging = FirebaseMessaging.instance;
+  static Logger logger = Logger();
 
   static Future<void> initMessaging() async {
     String token = await firebaseMessaging.getToken() ?? "-";
     print("TOKEN: $token");
+    FirebaseMessaging.onMessage.listen(_onMessage);
+  }
+
+  //DETECTAR EL MENSAJE CUANDO EL APP ESTA ABIERTO
+  static _onMessage(RemoteMessage message) {
+    if (message.notification != null) {
+      logger.i(message.notification!.title);
+      logger.d(message.notification!.body);
+    }
   }
 }
