093887f0b216d61ff0d8a55a059c8500fc520957
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2024-07-27 11:18:23 -0500

    nottifications

--- lib/utils/notification_service.dart
+++ lib/utils/notification_service.dart
@@ -9,10 +9,19 @@ class NotificationService {
     String token = await firebaseMessaging.getToken() ?? "-";
     print("TOKEN: $token");
     FirebaseMessaging.onMessage.listen(_onMessage);
+    FirebaseMessaging.onBackgroundMessage(_onBackgroundMessage);
   }
 
   //DETECTAR EL MENSAJE CUANDO EL APP ESTA ABIERTO
   static _onMessage(RemoteMessage message) {
+    if (message.notification != null) {
+      logger.i(message.notification!.title);
+      logger.d(message.notification!.body);
+    }
+  }
+
+  //DETECTAR CUANDO EL APP ESTA EN SEGUNDO PLANO
+  static Future _onBackgroundMessage(RemoteMessage message) async {
     if (message.notification != null) {
       logger.i(message.notification!.title);
       logger.d(message.notification!.body);
